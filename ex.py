3531a04b1aa16b70cc0c45c796ab0ded52e43a48
MarksAurelio <marksaurelio@yahoo.com>
2025-02-10 17:49:10 -0300

    criacao_novo_repositorio

---
+++ ex.py
@@ -0,0 +1,7 @@
+var1 = 1 # variável e seu valor
+var2 = 2 
+soma = var1 + var2 # variável com resultado
+
+print (soma) # impressão em tela da soma das variavéis
+print('O resultado é:', var1 + var2)
+print (f'O resultado é: {var1 + var2}', ) # f-print é o método que permite colocar uma variável dentro de uma string
