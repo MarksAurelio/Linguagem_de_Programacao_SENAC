f1a2684d4aa91c0c750d5e0f1fc9a26c1384f9c5
MarksAurelio <marksaurelio@yahoo.com>
2025-02-17 11:24:04 -0300

    update teste

--- exercicios/teste.py
+++ exercicios/teste.py
@@ -1,2 +1,13 @@
 palavra = 'marcos'
 print(palavra.capitalize())
+
+string = input('Insira uma palavra: ')
+primeiros_caracteres = string [:3]
+print(primeiros_caracteres)
+
+string = input('Insira uma palavra: ')
+ultimos_caracteres = string [-3:]
+print(ultimos_caracteres)
+
+for i in range(1, 11):
+    print(i)
