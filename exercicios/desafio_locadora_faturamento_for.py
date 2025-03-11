bc7370e275ecc6d4af28edb79832a2c12c20daa8
Marks <marksaurelio@yahoo.com>
2025-03-11 10:44:10 -0300

    Update desafio_locadora_faturamento_for.py

--- exercicios/desafio_locadora_faturamento_for.py
+++ exercicios/desafio_locadora_faturamento_for.py
@@ -1,7 +1,7 @@
-""" Faça uma programa que dados a quantidade de fitas que uma vídeo locadora possui 
+""" Faça uma programa que capta dados e a quantidade de fitas que uma vídeo locadora possui 
 e o valor que ela cobra por cada aluguel, informe:
-Sabendo que um terço das fitas são alugadas por mês,  qual o seu faturamento anual.
-Sabendo que quando o cliente atrasa a entrega, é cobrada uma multa de 10% sobre o valor do aluguel 
+Sabendo que um terço das fitas são alugadas por mês, qual o seu faturamento anual.
+Sabendo que quando o cliente atrasa a entrega é cobrada uma multa de 10% sobre o valor do aluguel. 
 Um décimo das fitas alugadas no mês são devolvidas com atraso é o valor ganho com multas por mês. """
 estoque = int(input('Estoque de fitas: '))
 valor_aluguel = float(input('Digite o valor do aluguel: '))
